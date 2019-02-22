4f61862a8c399c762906829c0225aca53d50f12e
AD\hecro15 <hector.rodriguez@liu.se>
2019-02-22 17:07:58 +0100

    Fix call to linreg_ols to remove non-existent input parameter

--- mynewrpackage/tests/testthat/test_linreg_s3.R
+++ mynewrpackage/tests/testthat/test_linreg_s3.R
@@ -3,15 +3,15 @@ context("linreg")
 data("iris")
 
 test_that("lenreg rejects errounous input", {
-  expect_error(linreg_ols(Petal.Length~Sepdsal.Width+Sepal.Length, data=iris,type=0))
-  expect_error(linreg_ols(Petal.Length~Sepdsal.Width+Sepal.Length, data=irfsfdis,type=0))
+  expect_error(linreg_ols(Petal.Length~Sepdsal.Width+Sepal.Length, data=iris))
+  expect_error(linreg_ols(Petal.Length~Sepdsal.Width+Sepal.Length, data=irfsfdis))
 })
 
 # linreg <- lm
 # 
 
 test_that("class is correct", {
-  linreg_mod <- linreg_ols(Petal.Length~Sepal.Width+Sepal.Length, data=iris,type=0)
+  linreg_mod <- linreg_ols(Petal.Length~Sepal.Width+Sepal.Length, data=iris)
   expect_s3_class(linreg_mod, "linreg")
 })
 
@@ -23,18 +23,18 @@ test_that("class is correct", {
 #})
 
 test_that("pred() works", {
-  linreg_mod <- linreg_ols(Petal.Length~Sepal.Width+Sepal.Length, data=iris,type=0)
+  linreg_mod <- linreg_ols(Petal.Length~Sepal.Width+Sepal.Length, data=iris)
   expect_equal(round(unname(pred(linreg_mod)[c(1,5,7)]),2), c(1.85, 1.53, 1.09))    
 })
 
 test_that("resid() works", {
-  linreg_mod <- linreg_ols(Petal.Length~Sepal.Width+Sepal.Length, data=iris,type=0)
+  linreg_mod <- linreg_ols(Petal.Length~Sepal.Width+Sepal.Length, data=iris)
   
   expect_equal(round(unname(resid(linreg_mod)[c(7,13,27)]),2), c(0.31, -0.58, -0.20))
 })
 
 test_that("coef() works", {
-  linreg_mod <- linreg_ols(Petal.Length~Sepal.Width+Sepal.Length, data=iris,type=0)
+  linreg_mod <- linreg_ols(Petal.Length~Sepal.Width+Sepal.Length, data=iris)
   expect_true(all(round(unname(coef(linreg_mod)),2) %in% c(-2.52, -1.34, 1.78)))
 })
 
